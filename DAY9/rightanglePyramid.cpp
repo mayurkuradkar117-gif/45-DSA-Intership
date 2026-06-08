89d35758c9dc78f7ceca3ea782b2fc4b2bcbb638
mayurkuradkar117-gif <mayurkuradkar117@gmail.com>
2026-06-08 15:30:01 +0530

    DAY9

---
+++ DAY9/rightanglePyramid.cpp
@@ -0,0 +1,21 @@
+#include<iostream>1
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    cout << "Enter the number of columns: ";
+    cin >> n;
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " "<<j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
